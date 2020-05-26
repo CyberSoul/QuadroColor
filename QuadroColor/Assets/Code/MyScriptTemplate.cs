07d3b70a770ea68952ed01828e667af8281e2218
Maksym Sliadniev <maksim.sliadniev@gmail.com>
2020-05-26 13:57:57 +0300

    +added script template

---
+++ QuadroColor/Assets/Code/MyScriptTemplate.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScriptTemplate : MonoBehaviour
+{
+    #region //SerializeFields
+    #endregion
+
+    #region //Events
+    #endregion
+
+    #region //Private fields
+    #endregion
+
+    #region //Properies
+    #endregion
+
+    #region //Overrides
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region //Public
+    #endregion
+
+    #region //Event callbacks
+    #endregion
+
+    #region //Private
+    #endregion
+
+    #region //Editor part
+    private void OnValidate()
+    {
+    }
+
+    private void OnDrawGizmos()
+    {
+    }
+
+    private void OnDrawGizmosSelected()
+    {   
+    }
+    #endregion
+}
